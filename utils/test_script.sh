e0026a3f1a2a8e52993581efd6bd3781a7438239
LukeTheWalker <lucag2001@gmail.com>
2024-02-20 15:41:02 +0100

    fix order argument

--- utils/test_script.sh
+++ utils/test_script.sh
@@ -9,7 +9,7 @@ compile() {
     local implementation=${implementations[$order]}
     # Add your compile command here, using the $implementation variable
     # For example: gcc -o program $implementation.c -Wall -Werror
-    make clean  >/dev/null  && make -j4 >/dev/null && ./bin/conj "$1" "$2" "$3" "$4" "$5" "$implementation"
+    make clean >/dev/null && make -j4 >/dev/null && ./bin/conj "$1" "$2" "$3" "$4" "$5" $order
 }
 
 # Main script
